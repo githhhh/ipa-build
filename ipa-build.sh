ed504aa3950d172812d8778ddea486be0ec5abe3
唐斌 <bin.tang@qyer.com>
2016-11-21 15:18:44 +0800

    fix xctool与xcode8 不兼容、添加命令行参数，指定configuration

--- ipa-build.sh
+++ ipa-build.sh
@@ -5,117 +5,116 @@
 #
 #  Created by 唐斌 on 14-4-21.
 #  Copyright (c) 2014年 project. All rights reserved.
-#!/bin/bash
 
-#执行git 命令
-#git st
-#exit
+#变量
+findWorkspacePath=""
+configurationModel=""
 
-
-workspaceExt=".xcworkspace"
-tempPath=""
-
-
+#添加configuration参数
+if [ -z $1 ];then
+    echo "\n [!] Miss option paramter \n"
+    echo "\t\t Did you option: Debug、Release 、Archive \n"
+    exit
+else
+    configuration_array=("Debug","Release","Archive")
+    if echo "${configuration_array[@]}" | grep -w "$1" &>/dev/null; then
+        configurationModel=$1
+    else
+        echo "\n [!] Unknown option: '$1'\n"
+        echo "\t\t Did you mean: Debug、Release 、Archive \n"
+        exit
+    fi
+fi
 
 #工程绝对路径
-#cd $1
 project_path=$(pwd)
 #build文件夹路径
 build_path=${project_path}/build
-
 #工程配置文件路径
 project_name=$(ls | grep xcodeproj | awk -F.xcodeproj '{print $1}')
-project_infoplist_path=${project_path}/${project_name}/${project_name}-Info.plist
-#取版本号
-bundleShortVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" ${project_infoplist_path})
-#取build值
-bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" ${project_infoplist_path})
-#取bundle Identifier前缀
-bundlePrefix=$(/usr/libexec/PlistBuddy -c "print CFBundleIdentifier" `find . -name "*-Info.plist"` | awk -F$ '{print $1}')
 
 
 cd $project_path
-echo "***clean  start**"
 
 #删除bulid目录
 if  [ -d ${build_path} ];then
-rm -rf ${build_path}
-
-echo "***clean build_path success***"
+    rm -rf ${build_path}
 fi
 
 #去掉xcode源码末尾的空格
 #find . -name "*.[hm]" | xargs sed -Ee 's/ +$//g' -i ""
 
-
 #find .xcworkspace
-for workspacePath in `find ${project_path} -name "$project_name$workspaceExt" -print`
+for workspacePath in `find ${project_path} -name "${project_name}.xcworkspace" -print`
 do
-tempPath=${workspacePath}
-break
+    findWorkspacePath=${workspacePath}
+    break
 done
 
-if [ "$tempPath" == "" ];then
+if [ "$findWorkspacePath" == "" ];then
+    #清理工程
+    xcodebuild  -project ${project_name}.xcodeproj \
+                -scheme ${project_name} \
+                -sdk iphoneos \
+                clean || exit
 
-#清理工程
-xctool  -project ${project_name}.xcodeproj \
-        -scheme ${project_name} \
-        -sdk iphoneos \
-        clean || exit
-
-#编译工程  Distribution  Release Debug
-xctool  -configuration Debug \
-        -sdk iphoneos \
-        -project ${project_path}/${project_name}.xcodeproj \
-        -scheme ${project_name} \
-         ONLY_ACTIVE_ARCH=NO \
-        TARGETED_DEVICE_FAMILY=1 \
-        DEPLOYMENT_LOCATION=YES CONFIGURATION_BUILD_DIR=${project_path}/build/Release-iphoneos || exit
+    #编译工程  Distribution  Release Debug
+    xcodebuild  -configuration $configurationModel \
+                -sdk iphoneos \
+                -project ${project_path}/${project_name}.xcodeproj \
+                -scheme ${project_name} \
+                 ONLY_ACTIVE_ARCH=NO \
+                TARGETED_DEVICE_FAMILY=1 \
+                DEPLOYMENT_LOCATION=YES CONFIGURATION_BUILD_DIR=${project_path}/build/Release-iphoneos || exit
 
 else
 
-#清理工程
-xctool  -workspace ${project_name}.xcworkspace \
-        -scheme ${project_name} \
-        -sdk iphoneos \
-        clean || exit
+    #清理工程
+    xcodebuild  -workspace ${project_name}.xcworkspace \
+                -scheme ${project_name} \
+                -sdk iphoneos \
+                clean || exit
 
-#编译工程  Distribution  Release Debug
-xctool  -configuration Debug  -workspace ${project_path}/${project_name}.xcworkspace \
-        -scheme ${project_name} \
-        -sdk iphoneos \
-        ONLY_ACTIVE_ARCH=NO \
-        TARGETED_DEVICE_FAMILY=1 \
-        DEPLOYMENT_LOCATION=YES CONFIGURATION_BUILD_DIR=${project_path}/build/Release-iphoneos  || exit
+    #编译工程  Distribution  Release Debug
+    xcodebuild  -configuration $configurationModel
+                -workspace ${project_path}/${project_name}.xcworkspace \
+                -scheme ${project_name} \
+                -sdk iphoneos \
+                ONLY_ACTIVE_ARCH=NO \
+                TARGETED_DEVICE_FAMILY=1 \
+                DEPLOYMENT_LOCATION=YES CONFIGURATION_BUILD_DIR=${project_path}/build/Release-iphoneos  || exit
 
 fi
-
 
 
 
 if [ -d ./ipa-build ];then
-rm -rf ipa-build
+    rm -rf ipa-build
 fi
+
 #打包
 cd $build_path
+
 mkdir -p ipa-build/Payload
+
 cp -r ./Release-iphoneos/*.app ./ipa-build/Payload/
+
 cd ipa-build
+
 zip -r ${project_name}.ipa *
 
 echo ${build_path}/ipa-build/${project_name}.ipa
+
 #找到桌面路径
 cd ~/Desktop
-#echo Desktop dir
-#echo $(pwd)
 #拷贝文件
 cp -r ${build_path}/ipa-build/${project_name}.ipa  $(pwd)
 #清空bulid目录
 cd ${build_path}/ipa-build
+#清理
 rm -rf Payload
-if  [ -d ${build_path} ];then
-rm -rf ${build_path}
-fi
-#done
 
+if  [ -d ${build_path} ];then
+    rm -rf ${build_path}
+fi
 
